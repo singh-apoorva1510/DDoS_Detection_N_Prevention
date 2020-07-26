fd54a48e89319386c82f217bfb0f2fc1b40de6d1
singh-apoorva1510 <59448607+singh-apoorva1510@users.noreply.github.com>
2020-07-26 17:49:41 +0530

    Create launchTraffic.py

---
+++ launchTraffic.py
@@ -0,0 +1,71 @@
+import sys
+import getopt
+import time
+from os import popen
+import logging
+logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
+from scapy.all import sendp, IP, UDP, Ether, TCP
+from random import randrange
+
+#start = ''
+#end = ''
+	
+def sourceIPgen():
+    not_valid = [10,127,254,1,2,169,172,192]
+
+    first = randrange(1,256)
+
+    while first in not_valid:
+        first = randrange(1,256)
+
+    ip = ".".join([str(first),str(randrange(1,256)),str(randrange(1,256)),str(randrange(1,256))])
+
+    return ip
+
+
+def gendest(start, end):
+
+    first = 10
+    second =0; third =0;
+    ip = ".".join([str(first),str(second),str(third),str(randrange(start,end))])
+   # print start
+   # print end
+    return ip
+
+#if __name__ == '__main__':
+  #main()
+
+def main(argv):
+    start = ''
+    end = ''
+    print argv
+    try:
+        opts, args = getopt.getopt(sys.argv[1:],'s:e:',['start=','end='])
+    except getopt.GetoptError:
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt =='-s':
+            start = int(arg)
+            print start
+        elif opt =='-e':
+            end = int(arg)
+            print end
+    if start == '':
+        print "Before start exit"
+        sys.exit()
+    if end == '':
+        print "Before end exit"
+        sys.exit()
+
+
+    interface = popen('ifconfig | awk \'/eth0/ {print $1}\'').read()
+
+    for i in xrange(1000):
+        print "HELOOOOO"
+        packets = Ether()/IP(dst=gendest(start, end),src=sourceIPgen())/UDP(dport=80,sport=2)
+        print(repr(packets))
+
+        sendp( packets,iface=interface.rstrip(),inter=0.1)
+
+if __name__ == '__main__':
+  main(sys.argv)
